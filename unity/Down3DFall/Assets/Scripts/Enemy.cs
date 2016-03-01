25fbe2eed928b76d861ce84e723be5bd1d21b8cb
blueblob0 <blueblob0@hotmail.com>
2016-03-01 17:15:20 +0000

    1
    1

---
+++ unity/Down3DFall/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    int speed = 100;
+    bool huntPlayer;
+    Transform player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (huntPlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("fdgdfg");
+            player = other.transform;
+            huntPlayer = true;
+        }
+    }
+}
