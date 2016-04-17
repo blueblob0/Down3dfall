b1a019a2633b6758f7cfc2e1aaf707cbc7e82d81
blueblob0 <blueblob0@hotmail.com>
2016-04-17 15:57:17 +0100

    1

---
+++ unity/Down3DFall/Assets/testscript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class testscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       // transform.localScale = Vector3.one * scaleSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
